ffbfee74437e3f0790d134d6c5437082b5f157b1
MathisHumbert <mathishumbert71@gmai.com>
2022-10-15 16:50:18 +0200

    navbar and header html css and gsap are done

--- main.js
+++ main.js
@@ -1,5 +1,58 @@
 import { gsap } from 'gsap';
+import { CustomEase } from 'gsap/all';
 import './styles/index.scss';
+
+gsap.registerPlugin(CustomEase);
+
+CustomEase.create('animation-smooth', '0.7, 0, 0.3, 1');
+CustomEase.create('nav-translate', '.9, 0, .1, 1');
+
+const init = () => {
+  initHeader();
+};
+
+const initHeader = () => {
+  const tl = gsap.timeline();
+
+  const headerTitles = gsap.utils.toArray('.header__container__title h1');
+
+  console.log(headerTitles);
+
+  tl.from(headerTitles, {
+    translateY: '125%',
+    rotate: '5deg',
+    stagger: 0.2,
+    ease: 'animation-smooth',
+    duration: 1.25,
+  })
+    .from(
+      '.nav__bar',
+      { scale: '(0, 1)', transformOrigin: 'left top', duration: 1.5 },
+      '<'
+    )
+    .from(
+      '.nav__bar__left, .nav__bar__right',
+      {
+        translateY: '50%',
+        opacity: 0,
+        ease: 'animation-smooth',
+        duration: 0.7,
+      },
+      '-=1'
+    )
+    .from(
+      '.header__bottom__left, .header__bottom__right',
+      {
+        translateY: '50%',
+        opacity: 0,
+        ease: 'animation-smooth',
+        duration: 0.7,
+      },
+      '<'
+    );
+};
+
+window.addEventListener('load', () => init());
 
 const nav = document.querySelector('.nav');
 const navButton = document.querySelector('.nav__right__hamburger');
@@ -19,8 +72,19 @@ navButton.addEventListener('click', (e) => {
         {
           translateY: '100%',
           rotate: '5deg',
-          stagger: 0.1,
-          ease: 'power2.easeInOut',
+          stagger: 0.05,
+          duration: 0.6,
+          ease: 'animation-smooth',
+        },
+        0
+      )
+      .to(
+        '.nav__bottom__left, .nav__bottom__right',
+        {
+          translateY: '-50%',
+          opacity: 0,
+          ease: 'animation-smooth',
+          duration: 0.7,
         },
         0
       )
@@ -29,11 +93,10 @@ navButton.addEventListener('click', (e) => {
         {
           translateY: 0,
           duration: 1.2,
-          ease: 'power2.easeInOut',
+          ease: 'nav-translate',
         },
-        '0.2'
+        0
       )
-
       .to('.nav__back__color', {
         background: '#f84525',
       })
@@ -50,10 +113,7 @@ navButton.addEventListener('click', (e) => {
       .to('.nav__hamburger__bar', { background: '#eafe07' }, '<');
   } else {
     nav.classList.add('open');
-    const animateMenuIn = gsap.timeline({
-      duration: 0.25,
-      ease: 'power2.easeInOut',
-    });
+    const animateMenuIn = gsap.timeline({ ease: 'none', duration: 0 });
 
     animateMenuIn
       .to(navLeftTitle, { color: '#1B1B1B' }, 0)
@@ -72,15 +132,31 @@ navButton.addEventListener('click', (e) => {
         {
           translateY: '100vh',
           duration: 1.2,
-          ease: 'power1.easeInOut',
+          ease: 'nav-translate',
         },
         0
       )
       .to('.nav__back__color', { background: '#eafe07' }, 0)
       .to(
         gsap.utils.toArray('.menu__item__text'),
-        { translateY: 0, rotate: 0, stagger: 0.1, ease: 'power2.easeInOut' },
-        '-=1'
+        {
+          translateY: 0,
+          rotate: 0,
+          stagger: 0.05,
+          ease: 'animation-smooth',
+          duration: 0.6,
+        },
+        '-=0.8'
+      )
+      .to(
+        '.nav__bottom__left, .nav__bottom__right',
+        {
+          translateY: 0,
+          opacity: 1,
+          ease: 'animation-smooth',
+          duration: 0.7,
+        },
+        '-=0.7'
       );
   }
 });
