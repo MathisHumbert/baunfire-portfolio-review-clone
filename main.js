37311eb1536907a9d0183c025cfed1374afef4b1
MathisHumbert <mathishumbert71@gmai.com>
2022-10-17 18:53:15 +0200

    worked on text and reviewers html, scss and gsap

--- main.js
+++ main.js
@@ -11,6 +11,189 @@ CustomEase.create('nav-translate', '.9, 0, .1, 1');
 const init = () => {
   initHeader();
   initPurpose();
+  initTextReviewers();
+};
+
+const initTextReviewers = () => {
+  const textSection = document.querySelector('.text__section');
+
+  const textSectionSubTitle = gsap.utils.toArray(
+    '.text__section__info__subtitle span'
+  );
+  const textSectionTitle = gsap.utils.toArray(
+    '.text__section__info__title span'
+  );
+
+  const button = document.querySelector('.rounded__button');
+  const buttonContainer = document.querySelector('.rounded__button__container');
+  const buttonText = document.querySelector('.rounded__button__text');
+
+  let diffBound = { x: 0, y: 0 };
+  let rootBound, itemBound;
+  let scale = 2;
+  let tollerance = 10;
+  let speed = 0.5;
+
+  button.addEventListener('mouseenter', function (e) {
+    gsap.killTweensOf(buttonContainer);
+    gsap.killTweensOf(buttonText);
+
+    rootBound = button.getBoundingClientRect();
+    itemBound = buttonContainer.getBoundingClientRect();
+    diffBound.x = (rootBound.width * scale - rootBound.width) / 2;
+    diffBound.y = (rootBound.height * scale - rootBound.height) / 2;
+  });
+
+  button.addEventListener('mouseleave', function (e) {
+    gsap.killTweensOf(buttonContainer);
+    gsap.killTweensOf(buttonText);
+
+    gsap.to([buttonContainer, buttonText], {
+      x: 0,
+      y: 0,
+      ease: 'elastic.out(1.1, .4)',
+      duration: 1.2,
+    });
+  });
+
+  button.addEventListener('mousemove', function (e) {
+    const x = e.clientX || e.touches[0].clientX;
+    const y = e.clientY || e.touches[0].clientY;
+
+    const maxX = ((rootBound.width - itemBound.width) / 2) * tollerance;
+    const maxY = ((rootBound.height - itemBound.height) / 2) * tollerance;
+
+    const newX = gsap.utils.mapRange(
+      0,
+      rootBound.width * scale,
+      -maxX,
+      maxX,
+      x - rootBound.x + diffBound.x
+    );
+
+    const newY = gsap.utils.mapRange(
+      0,
+      rootBound.height * scale,
+      -maxY,
+      maxY,
+      y - rootBound.y + diffBound.y
+    );
+
+    gsap.killTweensOf(buttonContainer);
+    gsap.killTweensOf(buttonText);
+
+    gsap.to(buttonContainer, {
+      x: newX * 10,
+      y: newY * 10,
+      ease: 'power3.out',
+      duration: speed,
+    });
+
+    gsap.to(buttonText, {
+      x: newX * 1.5,
+      y: newY * 1.5,
+      ease: 'power3.out',
+      duration: speed,
+    });
+  });
+
+  gsap.to(textSectionSubTitle, {
+    translateY: 0,
+    rotate: 0,
+    stagger: 0.1,
+    ease: 'animation-smooth',
+    duration: 1,
+    scrollTrigger: {
+      trigger: textSection,
+      start: '50% bottom',
+    },
+  });
+
+  gsap.to(textSectionTitle, {
+    translateY: 0,
+    rotate: 0,
+    stagger: 0.05,
+    ease: 'animation-smooth',
+    duration: 0.7,
+    scrollTrigger: {
+      trigger: textSection,
+      start: '50% bottom',
+    },
+  });
+};
+
+const initPurpose = () => {
+  const imageWrapperTop = document.querySelector(
+    '.purpose__images__top__wrapper'
+  );
+  const imageWrapperBottom = document.querySelector(
+    '.purpose__images__bottom__wrapper'
+  );
+  const imageRevealTop = imageWrapperTop.querySelector('.image__reveal');
+  const imageRevealBottom = imageWrapperBottom.querySelector('.image__reveal');
+  const imageTop = imageWrapperTop.querySelector('img');
+  const imageBottom = imageWrapperBottom.querySelector('img');
+
+  gsap.to('.purpose__header__title h2, .purpose__header__title h4 span', {
+    translateY: 0,
+    rotate: 0,
+    ease: 'animation-smooth',
+    duration: 0.7,
+    scrollTrigger: {
+      trigger: '.purpose__header__title',
+      start: 'bottom bottom',
+    },
+  });
+
+  gsap.to('.purpose__header__line', {
+    scale: 1,
+    duration: 1,
+    ease: 'linear',
+    scrollTrigger: {
+      trigger: '.purpose__header__title',
+      start: 'bottom bottom',
+    },
+  });
+
+  gsap.to('.purpose__container__text p', {
+    translateY: 0,
+    opacity: 1,
+    stagger: 0.1,
+    ease: 'animation-smooth',
+    duration: 0.7,
+    scrollTrigger: {
+      trigger: '.purpose__container__text',
+      start: 'bottom bottom',
+    },
+  });
+
+  const animatiteTopImages = gsap.timeline({
+    ease: 'nav-translate',
+  });
+
+  const animatiteBottomImages = gsap.timeline({
+    ease: 'nav-translate',
+  });
+
+  animatiteTopImages
+    .to(imageRevealTop, { translateY: '-100%', duration: 0.7 })
+    .to(imageTop, { scale: 1, duration: 1 }, 0);
+
+  animatiteBottomImages
+    .to(imageRevealBottom, { translateY: '-100%', duration: 0.7 })
+    .to(imageBottom, { scale: 1, duration: 1 }, 0);
+
+  ScrollTrigger.create({
+    trigger: imageWrapperTop,
+    start: '50% bottom',
+    animation: animatiteTopImages,
+  });
+
+  ScrollTrigger.create({
+    trigger: imageWrapperBottom,
+    start: '50% bottom',
+    animation: animatiteBottomImages,
+  });
 };
 
 const initHeader = () => {
@@ -50,41 +233,6 @@ const initHeader = () => {
       },
       '<'
     );
-};
-
-const initPurpose = () => {
-  gsap.to('.purpose__header__title h2, .purpose__header__title h4 span', {
-    translateY: 0,
-    rotate: 0,
-    ease: 'animation-smooth',
-    duration: 1,
-    scrollTrigger: {
-      trigger: '.purpose__header__title',
-      start: 'bottom bottom',
-    },
-  });
-
-  gsap.to('.purpose__header__line', {
-    scale: 1,
-    duration: 1,
-    ease: 'linear',
-    scrollTrigger: {
-      trigger: '.purpose__header__title',
-      start: 'bottom bottom',
-    },
-  });
-
-  gsap.to('.purpose__container__text p', {
-    translateY: 0,
-    opacity: 1,
-    stagger: 0.1,
-    ease: 'animation-smooth',
-    duration: 1,
-    scrollTrigger: {
-      trigger: '.purpose__container__text',
-      start: 'bottom bottom',
-    },
-  });
 };
 
 window.addEventListener('load', () => init());
@@ -165,7 +313,7 @@ navButton.addEventListener('click', (e) => {
       .to(
         '.nav__back',
         {
-          translateY: '100vh',
+          translateY: '91vh',
           duration: 1.2,
           ease: 'nav-translate',
         },
