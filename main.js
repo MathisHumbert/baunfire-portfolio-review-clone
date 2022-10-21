689edd8ebd64ff95b58db63283a2a6bb620f7433
MathisHumbert <mathishumbert71@gmai.com>
2022-10-21 18:56:58 +0200

    worked on reviewers html sccs and js

--- main.js
+++ main.js
@@ -22,11 +22,76 @@ gsap.registerPlugin(CustomEase, ScrollTrigger);
 CustomEase.create('animation-smooth', '0.7, 0, 0.3, 1');
 CustomEase.create('nav-translate', '.9, 0, .1, 1');
 
+const initReviewers = () => {
+  const animateInfinitSwiperFirst = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 11,
+    delay: 3,
+  });
+
+  const animateInfinitSwiperSecond = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 11,
+    delay: 7,
+  });
+
+  const animateInfinitSwiperThird = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 11,
+    delay: 11,
+  });
+
+  const images = gsap.utils.toArray('.reviewers__images__right__image img');
+
+  animateInfinitSwiperFirst
+    .to(images[0], { translateX: '-100%', duration: 1 })
+    .to(images[1], { translateX: 0, duration: 1 }, 0)
+    .to(images[2], { translateX: '100%', duration: 1 }, 0)
+    .to(images[2], { opacity: 1, duration: 0 })
+    .to(
+      images[0],
+      {
+        opacity: 0,
+        duration: 0,
+      },
+      '<'
+    );
+
+  animateInfinitSwiperSecond
+    .to(images[1], { translateX: '-100%', duration: 1 })
+    .to(images[2], { translateX: 0, duration: 1 }, 0)
+    .to(images[0], { translateX: '100%', duration: 1 }, 0)
+    .to(images[0], { opacity: 1, duration: 0 })
+    .to(
+      images[1],
+      {
+        opacity: 0,
+        duration: 0,
+      },
+      '<'
+    );
+
+  animateInfinitSwiperThird
+    .to(images[2], { translateX: '-100%', duration: 1 })
+    .to(images[0], { translateX: 0, duration: 1 }, 0)
+    .to(images[1], { translateX: '100%', duration: 1 }, 0)
+    .to(images[1], { opacity: 1, duration: 0 })
+    .to(
+      images[2],
+      {
+        opacity: 0,
+        duration: 0,
+      },
+      '<'
+    );
+};
+
 const init = () => {
   initMenu();
   initHeader();
   initPurpose();
   initTextButton();
+  initReviewers();
 };
 
 window.addEventListener('load', () => init());
