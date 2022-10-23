f8df9fc825e4d5429b06b970977edea9dae5016b
MathisHumbert <mathishumbert71@gmai.com>
2022-10-23 15:10:49 +0200

    refactor js, fixed image swiper animation, created bar swiper animation

--- main.js
+++ main.js
@@ -23,67 +23,100 @@ CustomEase.create('animation-smooth', '0.7, 0, 0.3, 1');
 CustomEase.create('nav-translate', '.9, 0, .1, 1');
 
 const initReviewers = () => {
+  gsap.registerEffect({
+    name: 'animateSwiperImage',
+    extendTimeline: true,
+    effect: (targets, config) => {
+      const tl = gsap.timeline();
+
+      tl.to(targets[config.first], { translateX: '-100%', duration: 1 })
+        .to(targets[config.second], { translateX: 0, duration: 1 }, 0)
+        .to(targets[config.third], { translateX: '100%', duration: 1 }, 0)
+        .to(targets[config.third], { opacity: 1, duration: 0 })
+        .to(
+          targets[config.first],
+          {
+            opacity: 0,
+            duration: 0,
+          },
+          '<'
+        );
+
+      return tl;
+    },
+  });
+
+  gsap.registerEffect({
+    name: 'animateSwiperBar',
+    extendTimeline: true,
+    effect: (target) => {
+      const tl = gsap.timeline();
+
+      tl.to(target, { opacity: 1, duration: 0, ease: 'none' }, 0);
+      tl.to(target, { scale: 1, duration: 3, ease: 'linear' }, 0);
+      tl.to(target, { opacity: 0, duration: 1, ease: 'none' });
+      tl.to(target, { scale: '(0, 1)', duration: 0 });
+
+      return tl;
+    },
+  });
+
+  const images = gsap.utils.toArray('.reviewers__images__right__image img');
+  const bars = gsap.utils.toArray('.reviewers__swiper__pagination__progress');
+
   const animateInfinitSwiperFirst = gsap.timeline({
     repeat: -1,
-    repeatDelay: 11,
+    repeatDelay: 10,
     delay: 3,
+  });
+  const animateInfinitBarFirst = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 7,
+    delay: 0,
   });
 
   const animateInfinitSwiperSecond = gsap.timeline({
     repeat: -1,
-    repeatDelay: 11,
+    repeatDelay: 10,
     delay: 7,
+  });
+  const animateInfinitBarSecond = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 7,
+    delay: 4,
   });
 
   const animateInfinitSwiperThird = gsap.timeline({
     repeat: -1,
-    repeatDelay: 11,
+    repeatDelay: 10,
     delay: 11,
   });
+  const animateInfinitBarThird = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 7,
+    delay: 8,
+  });
 
-  const images = gsap.utils.toArray('.reviewers__images__right__image img');
+  animateInfinitSwiperFirst.animateSwiperImage(images, {
+    first: 0,
+    second: 1,
+    third: 2,
+  });
+  animateInfinitBarFirst.animateSwiperBar(bars[0]);
 
-  animateInfinitSwiperFirst
-    .to(images[0], { translateX: '-100%', duration: 1 })
-    .to(images[1], { translateX: 0, duration: 1 }, 0)
-    .to(images[2], { translateX: '100%', duration: 1 }, 0)
-    .to(images[2], { opacity: 1, duration: 0 })
-    .to(
-      images[0],
-      {
-        opacity: 0,
-        duration: 0,
-      },
-      '<'
-    );
+  animateInfinitSwiperSecond.animateSwiperImage(images, {
+    first: 1,
+    second: 2,
+    third: 0,
+  });
+  animateInfinitBarSecond.animateSwiperBar(bars[1]);
 
-  animateInfinitSwiperSecond
-    .to(images[1], { translateX: '-100%', duration: 1 })
-    .to(images[2], { translateX: 0, duration: 1 }, 0)
-    .to(images[0], { translateX: '100%', duration: 1 }, 0)
-    .to(images[0], { opacity: 1, duration: 0 })
-    .to(
-      images[1],
-      {
-        opacity: 0,
-        duration: 0,
-      },
-      '<'
-    );
-
-  animateInfinitSwiperThird
-    .to(images[2], { translateX: '-100%', duration: 1 })
-    .to(images[0], { translateX: 0, duration: 1 }, 0)
-    .to(images[1], { translateX: '100%', duration: 1 }, 0)
-    .to(images[1], { opacity: 1, duration: 0 })
-    .to(
-      images[2],
-      {
-        opacity: 0,
-        duration: 0,
-      },
-      '<'
-    );
+  animateInfinitSwiperThird.animateSwiperImage(images, {
+    first: 2,
+    second: 0,
+    third: 1,
+  });
+  animateInfinitBarThird.animateSwiperBar(bars[2]);
 };
 
 const init = () => {
